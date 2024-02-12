bf5add969264c69ff7df68c082e68ae0ab7360ba
unknown <redoyhasan4585@gmail.com>
2024-02-12 23:10:13 +0400

    first commit

---
+++ conceptualSession/index.js
@@ -0,0 +1,10 @@
+const createdLi = document.createElement('li')
+createdLi.innerText='added by insert before';
+console.log(createdLi)
+
+const firstLi =document.getElementById('frist');
+
+const ul=document.querySelector('ul')
+
+// ul.insertBefore(createdLi, firstLi)
+// ul.removeChild(createdLi, firstLi)
